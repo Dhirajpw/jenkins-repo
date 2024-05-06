3297ae60e7e1f97518eeb0260fd32ace7e9220ff
Dhirajpw <dhirajpw92001@gmail.com>
2024-05-06 22:34:24 +0530

    svc added

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "eu-north-1"
 }
 
-resource "aws_vpc" "my_vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "192.168.0.0/16"
   tags = {
     Name = "my-vpc"
    }
 }
 
-resource "aws_subnet" "public_sub" {
-  vpc_id     = aws_vpc.my_vpc.id
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "192.168.0.0/24"
   map_public_ip_on_launch = true
   tags = {
@@ -18,8 +18,8 @@ resource "aws_subnet" "public_sub" {
   }
 }
 
-resource "aws_subnet" "private_sub" {
-  vpc_id     = aws_vpc.my_vpc.id
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.vpc.id
   cidr_block = "192.168.16.0/24"
 
   tags = {
@@ -27,8 +27,8 @@ resource "aws_subnet" "private_sub" {
   }
 }
 
-resource "aws_internet_gateway" "my_igw" {
-  vpc_id = aws_vpc.my_vpc.id
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
 
   tags = {
     Name = "my-igw"
@@ -36,17 +36,17 @@ resource "aws_internet_gateway" "my_igw" {
 }
 
 resource "aws_route" "my_route" {
-  route_table_id            = aws_vpc.my_vpc.default_route_table_id
+  route_table_id            = aws_vpc.vpc.default_route_table_id
   destination_cidr_block    = "0.0.0.0/0"
-  gateway_id                = aws_internet_gateway.my_igw.id
+  gateway_id                = aws_internet_gateway.igw.id
 }
 
-resource "aws_eip" "my_eip" {
+resource "aws_eip" "eip" {
   domain   = "vpc"
 }
 
-resource "aws_nat_gateway" "my_nat" {
-  allocation_id = aws_eip.my_eip.id
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.eip.id
   subnet_id     = aws_subnet.public_sub.id
 
   tags = {
@@ -55,32 +55,32 @@ resource "aws_nat_gateway" "my_nat" {
 }
 
 resource "aws_route_table" "private_route_t" {
-  vpc_id = aws_vpc.my_vpc.id
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.my_nat.id
+    gateway_id = aws_nat_gateway.nat.id
   }
 }
-resource "aws_route_table_association" "my_associat" {
-  subnet_id      = aws_subnet.private_sub.id
+resource "aws_route_table_association" "associate" {
+  subnet_id      = aws_subnet.private_subnet.id
   route_table_id = aws_route_table.private_route_t.id
 }
 
 
-resource "aws_instance" "demo_inst" {
+resource "aws_instance" "demo" {
   ami           = "ami-0d87226fbb4ce71fe"
   instance_type = "t3.micro"
-  subnet_id     = aws_subnet.public_sub.id
+  subnet_id     = aws_subnet.public_subnet.id
   security_groups = [aws_security_group.my_security.id ]
   tags = {
-    Name = "demo-inst"
+    Name = "demo"
   }
 }
 resource "aws_security_group" "my_security" {
   name        = "http"
   description = "Allow http"
-  vpc_id      = aws_vpc.my_vpc.id
+  vpc_id      = aws_vpc.vpc.id
 
   tags = {
     Name = "http"
